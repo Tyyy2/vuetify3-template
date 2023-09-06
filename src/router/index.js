40393b9c099246cf19d96f2cd3fc45f55c6b39ca
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-09-06 15:17:21 +0800

    tmp

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,14 @@ const routes = [
       {
         path: '',
         name: 'Home',
-        component: () => import('@/views/Home.vue'),
+        component: () => import('@/views/MainHome.vue'),
+      },
+      // 新增 Search 頁面路由設置
+      {
+        path: 'Search',
+        name: 'MainSearch',
+        // 動態載入(Lazy Loading)語法
+        component: () => import('@/views/MainSearch.vue'),
       },
       {
         path: 'Template',
@@ -21,13 +28,6 @@ const routes = [
             props: true
           }
         ]
-      },
-      // 新增 Search 頁面路由設置
-      {
-        path: 'Search',
-        name: 'Search',
-        // 動態載入(Lazy Loading)語法
-        component: () => import('@/views/Search.vue'),
       },
       // 新增 Search 頁面路由設置
       {
@@ -44,28 +44,26 @@ const routes = [
           }
         ]
       },
-
     ],
   },
   {
-        path: '/Test',
-        name: 'Test',
-        // 動態載入(Lazy Loading)語法
-        component: () => import('@/layouts/TestLayout.vue'),
-        children: [
-          {
-            path: '',
-            name: 'iframe',
-            component: () => import('@/views/Test/IFrame.vue'),
-            props: true
-          }
-        ]
-      },
+    path: '/Test',
+    name: 'Test',
+    // 動態載入(Lazy Loading)語法
+    component: () => import('@/layouts/TestLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'iframe',
+        component: () => import('@/views/Test/IFrame.vue'),
+        props: true
+      }
+    ]
+  },
 ]
 
 const router = createRouter({
-  // history: createWebHistory(process.env.BASE_URL), // 錯誤的寫法
-  history: createWebHistory(import.meta.env.BASE_URL), // 正確的寫法
+  history: createWebHistory(process.env.BASE_URL), // 錯誤的寫法
   routes,
 })
 export default router
---
+++ src/views/MainHome.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid class="fill-height align-start flex-column">
+    <VCard flat border class="w-100 px-3 py-2">
+      <v-row no-gutters align="center">
+        <v-col cols="12" md="6" lg="2">
+          <VTextField
+            label="請輸入"
+            density="compact"
+            hide-details
+            variant="outlined"
+          ></VTextField>
+        </v-col>
+        <v-col cols="12" md="6" lg="2" class="mt-1 mt-md-0">
+          <VTextField
+            label="請輸入"
+            density="compact"
+            hide-details
+            variant="outlined"
+          ></VTextField>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col cols="12" md="6" lg="auto" class="mt-1 mt-lg-0">
+          <VBtn variant="outlined">123</VBtn>
+        </v-col>
+      </v-row>
+    </VCard>
+    <VCard flat border class="mt-1 v-col">
+      <VTable>
+        <thead>
+          <tr>
+            <th class="text-left">項目1</th>
+            <th class="text-left">項目2</th>
+            <th class="text-left">項目3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in desserts" :key="item.name">
+            <td>{{ item.a }}</td>
+            <td>{{ item.b }}</td>
+            <td>{{ item.c }}</td>
+          </tr>
+        </tbody>
+      </VTable>
+    </VCard>
+  </v-container>
+</template>
+
+<script setup></script>
