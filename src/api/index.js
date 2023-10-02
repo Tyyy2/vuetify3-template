f8c879ecce5a8cff3ebd701a130ad79ff3ca4297
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-10-02 15:10:46 +0800

    axios mock adapter prototype ok

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,40 @@
 import { tokenObservers } from '@/store/auth';
 import axios from 'axios'
 import { useMeuns } from './menu';
+import { useMockMenus } from './testdata/menu';
+import MockAdapter from 'axios-mock-adapter'
 
 //#region create axios instance
-const baseURL = '/api/'
-const api = axios.create({ baseURL });
+const baseURL = import.meta.env.DEV ? '/' : 'https://yourapidomain.ktgh.com.tw/maybe/subfolder/';
+export const api = axios.create({ baseURL });
 
+function requestHandler(config){
 // 設定 api 送出前要做的事
-function requestHandler(){ }
+  return config;
+}
 api.interceptors.request.use(requestHandler)
 
-// 收到回應要做的事，例如收到 401, 402... 的動作
-function responseHandler(){ }
+function responseHandler(res){
+  // 收到回應要做的事，例如收到 401, 402... 的動作
+  return res;
+}
 api.interceptors.response.use(responseHandler)
-//#endregion
+// //#endregion
 
-// 當獲得 token 的時候把 token 加到 header
+// // 當獲得 token 的時候把 token 加到 header
 const tokenCallBack = (token) => api.defaults.headers.common['Authorization']= `Bearer ${token}`
 tokenObservers.push(tokenCallBack)
 
-//#region 這裡撰寫 api
+// //#region 這裡撰寫 api
 useMeuns(api) // 讓 api 有 useMenus 功能
-//#endregion
+// //#endregion
 
-// 使用 composable 寫法
+let mock = new MockAdapter(api)
+useMockMenus(mock) // [dev] 加入開發用的假 api middleware
+
+// // 使用 composable 寫法
 export const useApi = (app) =>{
   app.config.globalProperties.$axios = api
   app.config.globalProperties.$api = api
 }
-// https://ithelp.ithome.com.tw/articles/10230336
+// // https://ithelp.ithome.com.tw/articles/10230336
--- src/views/FrontDayjsx/IndexPage.vue
+++ src/views/FrontDayjsx/IndexPage.vue
@@ -1,5 +1,10 @@
 <template>
   <v-container fluid class="fill-height align-start flex-column">
+    <h4>
+      單純民國年加上 1911 推算西元時間計算 UTC+0 時會有錯誤，因為台灣 1937 - 1945 曾經是
+      GMT +9
+    </h4>
+    <h4>利用 day.js 即可正確計算</h4>
     <VCard class="mt-2 v-col">
       <v-row class="flex-column fill-height" no-gutters>
         <v-col>
@@ -28,44 +33,43 @@
 </template>
 
 <script setup>
-import dayx from '@/utils/dayx'
-import { computed } from 'vue'
-window.dayx = dayx
+import dayx from "@/utils/dayx";
+window.dayx = dayx;
 
-const d = dayx('0330315 123338', 'YYYMMDD HHmmss')
-const d2 = dayx('0330315', 'YYYMMSS')
+const d = dayx("0330315 123338", "YYYMMDD HHmmss");
+const d2 = dayx("0330315", "YYYMMSS");
 const items = [
   {
-    ori: '',
-    fmt: 'dayx 物件(當下時間)',
-    exp: 'dayx()',
+    ori: "",
+    fmt: "dayx 物件(當下時間)",
+    exp: "dayx()",
     val: dayx(),
   },
   {
     ori: 'dte: "0330315"',
-    fmt: 'dayx 物件',
+    fmt: "dayx 物件",
     exp: 'dayx("0330315", "YYYMMDD")',
     val: d2,
   },
   {
     ori: 'dte: "0330315", tme: "123338"',
-    fmt: 'dayx 物件',
+    fmt: "dayx 物件",
     exp: 'dayx("0330315 123338", "YYYMMDD HHmmss")',
     val: d,
   },
   {
-    ori: 'dayx物件 d',
-    fmt: '民國YYY年MM月DD日 HH:mm:ss',
+    ori: "dayx物件 d",
+    fmt: "民國YYY年MM月DD日 HH:mm:ss",
     exp: 'd.format("民國YYY年MM月DD日 HH:mm:ss")',
-    val: d.format('民國YYY年MM月DD日 HH:mm:ss'),
+    val: d.format("民國YYY年MM月DD日 HH:mm:ss"),
   },
   {
-    ori: 'dayx物件 d',
-    fmt: '年齡',
-    exp: 'd.age',
+    ori: "dayx物件 d",
+    fmt: "年齡",
+    exp: "d.age",
     val: d.age,
   },
-]
+];
 
 //   // //#region 從原生 Date
 //   // d = dayx("2022-03-15T12:33:38");
