5b66e53e8ab44e52fe2ce16d0baa0db9259cabac
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-10-16 10:22:51 +0800

    primiseDialog

---
+++ src/components/PromisDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog v-model="dialog" v-bind="$attrs">
+    <slot v-bind="{ Resolve }"></slot>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const dialog = ref(false);
+let resolve = null;
+
+const Resolve = (v) => {
+  resolve(v);
+  dialog.value = false;
+};
+
+const GetResult = async () => {
+  dialog.value = true;
+  return new Promise((res) => (resolve = res));
+};
+defineExpose({ GetResult, Resolve });
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Admin/IndexPage.vue
+++ src/views/Admin/IndexPage.vue
@@ -1,7 +1,24 @@
 <template>
-  <div></div>
+  <PromisDialog ref="dialog" v-slot="{ Resolve }" :width="500">
+    <v-card>
+      <v-card-text>
+        <v-text-field label="輸入框" v-model="msg"></v-text-field>
+        <v-btn @click="Resolve(msg)">Resolve</v-btn>
+      </v-card-text>
+    </v-card>
+  </PromisDialog>
 </template>
 
-<script setup></script>
+<script setup>
+import PromisDialog from "@/components/PromisDialog.vue";
+import { ref } from "vue";
+const dialog = ref(null);
+const msg = ref("");
+const test = async () => {
+  var a = await dialog.value?.GetResult();
+  console.log(a);
+};
+window.test = test;
+</script>
 
 <style lang="scss" scoped></style>
