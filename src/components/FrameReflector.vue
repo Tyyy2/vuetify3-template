15d8df005f2df490956e23e157d5203b98b73582
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-10-23 13:58:03 +0800

    throttle

--- src/components/FrameReflector.vue
+++ src/components/FrameReflector.vue
@@ -1,27 +1,29 @@
 <template>
   <iframe ref="holder" class="w-100 h-100 d-block" style="overflow: hidden"></iframe>
-  <iframe :style="(displayerStyle as any)" :src="modelValue || ''"></iframe>
+  <iframe :style="displayerStyle" :src="modelValue || ''"></iframe>
   <!-- sandbox="allow-same-origin allow-scripts allow-top-navigation" -->
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { computed, ref, onMounted, onUnmounted } from "vue";
+import _ from "lodash";
 
-type Props = {
-  modelValue: string | null;
-  virtualWidth: number;
-};
-const props = withDefaults(defineProps<Props>(), {
+const props = defineProps({
   virtualWidth: 1920,
+  modelValue: {
+    type: String,
+    default: null,
+  },
 });
 defineEmits(["update:modelValue"]);
 
-const holder = ref<HTMLIFrameElement | null>(null);
-const holderHeight = ref<number>(0);
-const holderWidth = ref<number>(0);
-const top = ref<number>(0);
-const left = ref<number>(0);
-const resizeHandler = () => {
+const holder = ref(null);
+const holderHeight = ref(0);
+const holderWidth = ref(0);
+const top = ref(0);
+const left = ref(0);
+const resizeHandler = _.throttle(() => {
+  console.log("resizeHandler");
   const h = holder.value?.clientHeight;
   const w = holder.value?.clientWidth;
   holderWidth.value = w ?? 0;
@@ -29,7 +31,7 @@ const resizeHandler = () => {
   const position = holder.value?.getBoundingClientRect();
   top.value = position?.top ?? 0;
   left.value = position?.left ?? 0;
-};
+}, 350);
 
 onMounted(() => resizeHandler());
 onMounted(() => holder.value.contentWindow.addEventListener("resize", resizeHandler));
@@ -39,7 +41,7 @@ onUnmounted(() =>
 // 原始寬度 985px 可以塞得下 frame content
 const displayerStyle = computed(() => {
   const { virtualWidth } = props;
-  const scale: string = ((holderWidth.value ?? virtualWidth) / virtualWidth).toFixed(3);
+  const scale = ((holderWidth.value ?? virtualWidth) / virtualWidth).toFixed(3);
   const height = (holderHeight.value / parseFloat(scale)).toFixed(2);
   return {
     border: "1px solid grey",
